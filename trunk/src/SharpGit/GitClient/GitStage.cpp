5dace084c63351a16f7f5bc5efbcdf8b0d18012c
rhuijben <rhuijben@2820370b-d19e-42a0-aca3-c528b2ab96fc>
2014-05-22 10:23:08 +0000

    Make GitClient.Add() an alias for GitClient.Stage() and move the implementation over.
    * src/SharpGit/SharpGit.vcxproj.filters
    * src/SharpGit/SharpGit.vcxproj
    * src/SharpGit/Plumbing/GitIndex.h
    * src/SharpGit/Plumbing/GitIndex.cpp
    * src/SharpGit/GitClient/GitStageArgs.h
    * src/SharpGit/GitClient/GitStage.h
    * src/SharpGit/GitClient/GitStage.cpp
    * src/SharpGit/GitClient/GitClient.h
    * src/SharpGit/GitClient/GitAddArgs.h
    * src/SharpGit/GitClient/GitAdd.cpp
      Use .Stage() for staging changes.

--- trunk/src/SharpGit/GitClient/GitStage.cpp
+++ trunk/src/SharpGit/GitClient/GitStage.cpp
@@ -1,8 +1,36 @@
 #include "stdafx.h"
 #include "GitClient.h"
-#include "GitStage.h"
+#include "GitStageArgs.h"
+
+#include "Plumbing/GitRepository.h"
+#include "Plumbing/GitIndex.h"
 
 using namespace SharpGit;
+using namespace SharpGit::Plumbing;
+
+void GitClient::AssertNotBare(GitRepository ^repository)
+{
+    if (! repository)
+        throw gcnew ArgumentNullException("repository");
+
+    if (repository->IsBare)
+        throw gcnew InvalidOperationException("Not valid on a bare repository");
+}
+
+void GitClient::AssertNotBare(GitRepository %repository)
+{
+    AssertNotBare(%repository);
+}
+
+bool GitClient::Add(String ^path)
+{
+    return Stage(path, gcnew GitStageArgs());
+}
+
+bool GitClient::Add(String ^path, GitStageArgs ^args)
+{
+    return Stage(path, args);
+}
 
 bool GitClient::Stage(String ^path)
 {
@@ -11,6 +39,9 @@ bool GitClient::Stage(String ^path)
 
 bool GitClient::Stage(String ^path, GitStageArgs^ args)
 {
+    if (String::IsNullOrEmpty(path))
+        throw gcnew ArgumentNullException("path");
+
 	return Stage(gcnew array<String^> { path }, args);
 }
 
@@ -21,5 +52,31 @@ bool GitClient::Stage(IEnumerable<String^>^ paths)
 
 bool GitClient::Stage(IEnumerable<String^>^ paths, GitStageArgs^ args)
 {
-	return false;
+    if (! paths)
+        throw gcnew ArgumentNullException("path");
+    else if (! args)
+        throw gcnew ArgumentNullException("args");
+
+    GitRepository repo;
+
+    for each (String ^ p in paths)
+    {
+        if (!repo.Locate(p, args))
+            return false;
+    }
+
+    if (repo.IsDisposed)
+        return false;
+
+    AssertNotBare(repo);
+
+    GitPool pool(_pool);
+
+    for each (String ^path in paths)
+    {
+        if (!repo.Index->Add(repo.MakeRelpath(path, %pool), args, %pool))
+            return false;
+    }
+    
+    return repo.Index->Write(args);
 }
