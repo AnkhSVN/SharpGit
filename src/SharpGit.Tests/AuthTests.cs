a52ddaccf6d6931d7950a9c3aa44f04c8d9c26f2
rhuijben <rhuijben@2820370b-d19e-42a0-aca3-c528b2ab96fc>
2015-01-13 21:38:21 +0000

    * src/SharpGit.Tests/AuthTests.cs   Add missing file

---
+++ src/SharpGit.Tests/AuthTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using SharpGit.Diff;
+using Assert = NUnit.Framework.Assert;
+
+namespace SharpGit.Tests
+{
+    [TestClass]
+    public class AuthTests : TestBase
+    {
+
+    }
+}
